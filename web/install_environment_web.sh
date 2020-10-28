2efa008019594021663d4e85557c0ba7c79c38e3
CebulowyNinja <jan@olencki.pl>
2020-10-28 02:15:45 +0100

    Update install web enviroment script

--- web/install_environment_web.sh
+++ web/install_environment_web.sh
@@ -26,24 +26,24 @@ sudo apt-get autoremove
 #############################################################################
 ## Install Node.js
 echo "!  Install Node.js"
-curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
+curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh # This script identify distro, add apt source and run apt-get update
 sudo apt install nodejs
 
 ## Check instaled version
-node_expected_version="v10.22.0"
+node_expected_version="v14.15"
 node_version=`nodejs -v`
-if [[ $node_version == $node_expected_version ]]; then
+if [[ $node_version == $node_expected_version* ]]; then
     echo "!  Node.js $node_version installed "
 else
-    echo "ERROR: Incorrect Node.js version - $node_version (expected $node_expected_version)"
+    echo "ERROR: Incorrect Node.js version - $node_version (expected $node_expected_version.x)"
 fi
-npm_expected_version="6.14.6"
+npm_expected_version="6.14"
 npm_version=`npm -v`
-if [[ $npm_version == $npm_expected_version ]]; then
+if [[ $npm_version == $npm_expected_version* ]]; then
     echo "!  NPM v$npm_version installed"
 else
-    echo "ERROR: Incorrect NPM version - v$npm_version (expected v$npm_expected_version)"
+    echo "ERROR: Incorrect NPM version - v$npm_version (expected v$npm_expected_version.x)"
 fi
 
 ## Clean up
