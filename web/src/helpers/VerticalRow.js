9b76c26d0491609dad2bb3eb378ac4c50ea4ac7f
CebulowyNinja <jan@olencki.pl>
2020-10-29 20:29:06 +0100

    Refactor vertical helpers

---
+++ web/src/helpers/VerticalRow.js
@@ -0,0 +1,12 @@
+import './VerticalRow.scss';
+
+function VerticalRow(props) {
+    const stylesRow = {flexBasis: props.basis};
+    return (
+        <div className="vertical-row" style={stylesRow}>
+            {props.children}
+        </div>
+    )
+}
+
+export default VerticalRow
