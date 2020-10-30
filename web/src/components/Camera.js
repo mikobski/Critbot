8c918bf79895f448b6cfda6d96a21cca80e8399a
CebulowyNinja <jan@olencki.pl>
2020-10-30 21:05:43 +0100

    Sketch of camera

--- web/src/components/Camera.js
+++ web/src/components/Camera.js
@@ -1,9 +1,35 @@
 import React from "react";
+import Canvas from "../helpers/Canvas";
 
 class Camera extends React.Component {
+  _topic;
+  _data = null;
+
+  componentDidMount() {
+    const topicManager = this.props.ros.topic;
+    const topicName = this.props.topic;
+    this._topic = topicManager.subscribe(topicName, "turtlesim/Pose", this.topicListener);
+  }
+  componentWillUnmount() {
+    this._topic.dispose();
+  }
+
+  topicListener = (message) => {
+    const rawData = new Blob([message.data], { type: `image/${message.format}` })
+    this._data = createImageBitmap(rawData);
+  }
+
+  draw = (ctx, frameCount) => {
+    if(this._data) {
+      ctx.drawImage(this._data, 0, 0);
+    }
+  }
+
   render () {
     return (
-      <div>Camera</div>
+      <div style={{ border: "1px solid red"}}>
+        <Canvas draw={ this.draw }/>
+      </div>
     );
   }
 }
