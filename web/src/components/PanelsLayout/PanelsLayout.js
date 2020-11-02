16ad6785f8d2425c2980f0ee27b6c5c7d26a207b
CebulowyNinja <jan@olencki.pl>
2020-11-03 00:27:16 +0100

    Work in progress, but stable

--- web/src/components/PanelsLayout/PanelsLayout.js
+++ web/src/components/PanelsLayout/PanelsLayout.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Camera from "components/Camera";
 import Lidar from "components/Lidar";
 import ManualControl from "components/ManualControl/ManualControl";
@@ -9,70 +10,81 @@ import { ROS_CONFIG } from "utils/RosConfig";
 import { MODE } from "utils/Mode";
 import "components/PanelsLayout/PanelsLayout.scss";
 
-const PanelsLayout = (props) => {
-  const StylesContainer = {
-    height: "100%",
-    display: "flex",
-    flexDirection: "column"
+class PanelsLayout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.panelsContainer = React.createRef();
   }
 
-  return (
-    <div className="Panels Panels-container-col">
-      <div className="Panels-col Panels-container">
-        <div className="Panels-panel" style={{ 
-            flexBasis: "50%",
-            borderBottomWidth: ".15rem",
-            borderRightWidth: ".15rem"
-          }}>
-          <Camera topic={ ROS_CONFIG.defaultTopics.camera }/> 
-        </div>  
-        <div className="Panels-panel" style={{ 
-            flexBasis: "50%",
-            flexGrow: "1",
-            borderTopWidth: ".15rem",
-            borderRightWidth: ".15rem"
-          }}>
-          <Lidar topic={ ROS_CONFIG.defaultTopics.lidar }
-            physicalWidth="22" physicalHeight="22"
-            gridMax="10" gridCount="4"/>
-        </div>
-      </div>
-      <div className="Panels-col">
-        <div style={{flexBasis: "50%"}} style={StylesContainer}>
-          <div class="Panels-container-col">
-            <div className="Panels-col Panels-container">
-              <div className="Panels-panel" style={{ 
-                flexBasis: "50%",              
-                borderRightWidth: ".15rem",
-                borderBottomWidth: ".15rem",
-                borderLeftWidth: ".15rem",
-                padding: "0 1rem"
-              }}>
-                <StatusDetailed/>
-                <EmergencyStop/>
-                  {
-                    props.mode == MODE.manual && 
-                    <ManualControl topic={ ROS_CONFIG.defaultTopics.manualControl } /> 
-                  }
-              </div>
-            </div>
-            <div className="Panels-col">
-            <div className="Panels-panel" style={{            
-                height: "100%",
-                borderLeftWidth: ".15rem",
-                borderBottomWidth: ".15rem"
-              }}>
-                <Map/>
-              </div>
-            </div>
-          </div>
-          <div style={{flexBasis: "100%"}} >
-            <Measurements/>
+  render() {
+    const StylesContainer = {
+      height: "100%",
+      display: "flex",
+      flexDirection: "column"
+    }
+
+    return (
+      <div ref={ this.panelsContainer } className="Panels Panels-container-col">
+        <div className="Panels-col Panels-container">
+          <div className="Panels-panel" style={{ 
+              flexBasis: "50%",
+              borderBottomWidth: ".15rem",
+              borderRightWidth: ".15rem"
+            }}>
+            <Camera topic={ ROS_CONFIG.defaultTopics.camera }/> 
+          </div>  
+          <div className="Panels-panel" style={{ 
+              flexBasis: "50%",
+              flexGrow: "1",
+              borderTopWidth: ".15rem",
+              borderRightWidth: ".15rem"
+            }}>
+            <Lidar topic={ ROS_CONFIG.defaultTopics.lidar }
+              physicalWidth="22" physicalHeight="22"
+              gridMax="10" gridCount="4"/>
           </div>
         </div>
+        <div className="Panels-col">
+          <div style={{flexBasis: "50%"}} style={StylesContainer}>
+            <div class="Panels-container-col">
+              <div className="Panels-col Panels-container">
+                <div className="Panels-panel" style={{ 
+                  flexBasis: "50%",              
+                  borderRightWidth: ".15rem",
+                  borderBottomWidth: ".15rem",
+                  borderLeftWidth: ".15rem",
+                  padding: "0 1rem"
+                }}>
+                  <StatusDetailed/>
+                  <EmergencyStop/>
+                    {
+                      this.props.mode == MODE.manual && 
+                      <ManualControl topic={ ROS_CONFIG.defaultTopics.manualControl } /> 
+                    }
+                </div>
+              </div>
+              <div className="Panels-col">
+                <div className="Panels-panel" style={{            
+                    height: "100%",
+                    borderLeftWidth: ".15rem",
+                    borderBottomWidth: ".15rem"
+                  }}>
+                    <Map/>
+                  </div>
+                </div>
+            </div>
+            <div className="Panels-panel" style={{
+              flexBasis: "100%",
+              borderLeftWidth: ".15rem",
+              borderTopWidth: ".15rem"
+            }} >
+              <Measurements/>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default PanelsLayout;
