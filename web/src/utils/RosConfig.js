2281d39299e926ad018847d745415126c54cad76
CebulowyNinja <jan@olencki.pl>
2020-11-03 20:04:20 +0100

    Fix error on reconnect topic

--- web/src/utils/RosConfig.js
+++ web/src/utils/RosConfig.js
@@ -2,6 +2,6 @@ export const ROS_CONFIG = {
   defaultTopics: {
     camera: "/d400/color/image_raw/compressed",
     lidar: "/scan",
-    manualControl: "/turtle1/cmd_vel"
+    manualControl: ""
   }
 };
