0d334015991a47f4502c3cd46d9dff00501f7db4
CebulowyNinja <jan@olencki.pl>
2020-11-06 21:18:44 +0100

    Refactor PanelsLayout.js

--- web/src/components/PanelsLayout/PanelsLayout.js
+++ web/src/components/PanelsLayout/PanelsLayout.js
@@ -38,36 +38,22 @@ class PanelsLayout extends React.Component {
   render() {
     return (
       <div ref={ this.panelsContainer } className="Panels Panels-container-col">
-        <div  ref={ this.columnLeft }className="Panels-col Panels-container">
-          <div ref={ this.cameraPanel } className="Panels-panel" style={{ 
-              flexBasis: "50%",
-              borderBottomWidth: ".15rem",
-              borderRightWidth: ".15rem"
-            }}>
+        <div ref={ this.columnLeft } className="Panels-col Panels-container Panels-brd-right" 
+             style={{flexBasis: "50%" }}>
+          <div ref={ this.cameraPanel } className="Panels-panel" style={{ flexBasis: "50%" }}>
             <Camera topic={ ROS_CONFIG.defaultTopics.camera }/> 
           </div>  
-          <div className="Panels-panel" style={{ 
-              borderTopWidth: ".15rem",
-              borderRightWidth: ".15rem"
-            }}>
+          <div className="Panels-panel Panels-brd-top">
             <Lidar topic={ ROS_CONFIG.defaultTopics.lidar }
               physicalWidth="22" physicalHeight="22"
               gridMax="10" gridCount="4"/>
           </div>
         </div>
-        <div className="Panels-col">
+        <div className="Panels-col" style={{flexBasis: "50%" }}>
           <div style={{flexBasis: "50%" }} className="Panels-container">
             <div className="Panels-container-col">
-              <div className="Panels-col Panels-container" style={{
-                flexBasis: "0px"
-              }}>
-                <div className="Panels-panel" style={{ 
-                  flexBasis: "100%",              
-                  borderRightWidth: ".15rem",
-                  borderBottomWidth: ".15rem",
-                  borderLeftWidth: ".15rem",
-                  padding: "0 1rem"
-                }}>
+              <div className="Panels-col Panels-container" style={{ flexBasis: "0px" }}>
+                <div className="Panels-panel Panels-container Panels-brd-right" style={{ flexBasis: "100%", padding: "0 1rem" }}>
                   <StatusDetailed/>
                   <EmergencyStop/>
                     {
@@ -76,23 +62,13 @@ class PanelsLayout extends React.Component {
                     }
                 </div>
               </div>
-              <div className="Panels-col" style={{
-                flexBasis: "100%"
-              }}>
-                <div className="Panels-panel" style={{            
-                    height: "100%",
-                    borderLeftWidth: ".15rem",
-                    borderBottomWidth: ".15rem"
-                  }}>
-                    <Map/>
-                  </div>
+              <div className="Panels-col" style={{ flexBasis: "100%" }}>
+                <div className="Panels-panel" style={{ height: "100%" }}>
+                  <Map/>
                 </div>
+              </div>
             </div>
-            <div className="Panels-panel" style={{
-              flexBasis: "100%",
-              borderLeftWidth: ".15rem",
-              borderTopWidth: ".15rem"
-            }} >
+            <div className="Panels-panel Panels-brd-top" style={{ flexBasis: "100%" }} >
               <Measurements/>
             </div>
           </div>
