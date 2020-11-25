b4779c2eb3188ef091f3deef3d23dc6510f0e36e
mikobski <mikbarcikowski@gmail.com>
2020-11-25 18:32:14 +0100

    changed topic names

--- robot/src/web_robot_communication/nodes/manual_mode.py
+++ robot/src/web_robot_communication/nodes/manual_mode.py
@@ -5,14 +5,14 @@ from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 
 value = 0
-pub = rospy.Publisher('/setpoint_velocity/cmd_vel_unstamped', Twist, queue_size=10, latch=True)
+pub = rospy.Publisher('/mavros/setpoint_velocity_unstamped', Twist, queue_size=10, latch=True)
 
 def callback(data):
     rospy.loginfo(data)
     pub.publish(data)
 
 def listener():
-    rospy.Subscriber("/turtle1/cmd_vel", Twist, callback)  
+    rospy.Subscriber("/web/manual_control", Twist, callback)  
     rospy.spin()
 
 if __name__ == '__main__':
