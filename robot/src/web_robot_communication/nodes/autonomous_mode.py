0d4dd9a5f194338000d19fdd15638582d9f1b07e
mikobski <44801719+mikobski@users.noreply.github.com>
2020-11-25 18:33:38 +0100

    Feature web robot communication (#21)
    * WIP script to changing mode
    
    * working mode change
    
    * removed trash files
    
    * deleted pyc file
    
    * changed topic names

---
+++ robot/src/web_robot_communication/nodes/autonomous_mode.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+
+value = 0
+pub = rospy.Publisher('/setpoint_velocity/auto', Twist, queue_size=10, latch=True)
+
+def callback(data):
+    rospy.loginfo(data)
+    pub.publish(data)
+
+def listener():
+    rospy.Subscriber("/turtle1/cmd_vel", Twist, callback)  
+    rospy.spin()
+
+if __name__ == '__main__':
+    rospy.init_node('listener_and_publisher', anonymous=True)
+    listener()
+
---
+++ robot/src/web_robot_communication/nodes/mode_changes.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from web_robot_communication.srv import ModeChanges,ModeChangesResponse
+import rospy
+import std_msgs.msg
+import sys
+import re
+import subprocess
+import psutil
+
+MANUAL_MODE_SCRIPT = 'manual_mode.py'
+AUTONOMOUS_MODE_SCRIPT = 'autonomous_mode.py'
+PGREP_CMD = 'pgrep -af '
+
+def check_if_script_running(mode):
+    output = subprocess.check_output(PGREP_CMD+mode, shell=True)
+    splitted_output = output.split()
+    print(splitted_output)
+    if splitted_output[2] == mode:
+        return psutil.Process(int(splitted_output[0]))
+    else:
+        return None
+
+def handle_mode_changes(req):
+    autonomous_check = check_if_script_running(AUTONOMOUS_MODE_SCRIPT)
+    manual_check = check_if_script_running(MANUAL_MODE_SCRIPT)
+    mode_type = re.search('\"(.+?)\"', str(req)).group(1)
+    if mode_type in ['autonomous']:
+        if manual_check is None:
+            print('That script is not running')
+        else:
+            manual_check.terminate()
+        if autonomous_check is None:
+            print('That script is not running')
+            script = subprocess.Popen([sys.executable, AUTONOMOUS_MODE_SCRIPT], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            return ModeChangesResponse('Mode changed to autonomous')
+        else:
+            return ModeChangesResponse('Mode autonomous already running') 
+             
+    elif mode_type in ['manual']:
+        if autonomous_check is None:
+            print('That script is not running')
+        else:
+            autonomous_check.terminate()
+        if manual_check is None:
+            print('That script is not running')
+            script = subprocess.Popen([sys.executable, MANUAL_MODE_SCRIPT], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            return ModeChangesResponse('Mode changed to manual')
+        else:
+            return ModeChangesResponse('Mode autonomous already running') 
+    else:
+        return ModeChangesResponse('This kind of mode does not exist')
+    
+
+def mode_changes_server():
+    rospy.init_node('mode_changes_server')
+    s = rospy.Service('mode_changes', ModeChanges, handle_mode_changes)
+    print("Ready to change mode.")
+    rospy.spin()
+
+if __name__ == "__main__":
+    
+    mode_changes_server()
