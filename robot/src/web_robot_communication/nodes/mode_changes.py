fcea3f1a6fcd58627d3ca0b07a7ac92f1eb1e9df
mikobski <mikbarcikowski@gmail.com>
2021-01-08 14:19:51 +0100

    update mode changes

--- robot/src/web_robot_communication/nodes/mode_changes.py
+++ robot/src/web_robot_communication/nodes/mode_changes.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from __future__ import print_function
 from web_robot_communication.srv import ModeChanges,ModeChangesResponse
@@ -14,9 +14,8 @@ import subprocess
 import psutil
 
 GUIDED_MODE_SCRIPT = 'guided_mode.py'
-#AUTONOMOUS_MODE_SCRIPT = 'autonomous_mode.py'
 PGREP_CMD = 'pgrep -af '
-pub = rospy.Publisher('/mavros/global_position/set_gp_origin', GeoPointStamped, queue_size=10, latch=True)
+#pub = rospy.Publisher('/mavros/global_position/set_gp_origin', GeoPointStamped, queue_size=10, latch=True)
 
 def check_if_script_running(mode):
     output = subprocess.check_output(PGREP_CMD+mode, shell=True)
@@ -29,50 +28,27 @@ def check_if_script_running(mode):
 
 
 def handle_mode_changes(req):
-    #autonomous_check = check_if_script_running(AUTONOMOUS_MODE_SCRIPT)
-    header = Header()
-    header.stamp = rospy.get_rostime()
-    header.frame_id = ''
-    header.seq = 0
-    pos = GeoPointStamped()
-    pos.header = header
-    pos.position.latitude = 54.0
-    pos.position.longitude = 18.0
-    pos.position.altitude = 10.0
-    pub.publish(pos)
+        #    header = Header()
+        #    header.stamp = rospy.get_rostime()
+        #    header.frame_id = ''
+        #    header.seq = 0
+        #    pos = GeoPointStamped()
+        #    pos.header = header
+        #    pos.position.latitude = 54.0
+        #    pos.position.longitude = 18.0
+        #    pos.position.altitude = 10.0
+        #    pub.publish(pos)
     guided_check = check_if_script_running(GUIDED_MODE_SCRIPT)
     mode_type = re.search('\"(.+?)\"', str(req)).group(1)
-    # if mode_type in ['autonomous']:
-    #     if guided_check is None:
-    #         print('That script is not running')
-    #     else:
-    #         guided_check.terminate()
-    #     if autonomous_check is None:
-    #         print('That script is not running') 
-    #         rospy.wait_for_service('set_mode')           
-    #         try:
-    #             guided_srv = rospy.ServiceProxy('set_mode', SetMode)
-    #             resp = guided_srv(base_mode=0, custom_mode=15)
-    #             script = subprocess.Popen([sys.executable, AUTONOMOUS_MODE_SCRIPT], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    #             return ModeChangesResponse('Mode changed to autonomous')
-    #         except rospy.ServiceException as e:
-    #             print("Service call failed: %s"%e)
-    #     else:
-    #         return ModeChangesResponse('Mode autonomous already running')              
+    rospy.wait_for_service('/mavros/set_mode')
+    mode_change_srv = rospy.ServiceProxy('/mavros/set_mode', SetMode)       
     if mode_type in ['guided']:
-        # if autonomous_check is None:
-        #     print('That script is not running')
-        # else:
-        #     autonomous_check.terminate()
         if guided_check is None:
             print('That script is not running')
-            rospy.wait_for_service('/mavros/set_mode')
             print('1')
             try:
-                print('2')
-                guided_srv = rospy.ServiceProxy('/mavros/set_mode', SetMode)
                 print('3')
-                resp = guided_srv(base_mode=0, custom_mode='GUIDED')
+                resp = mode_change_srv(base_mode=0, custom_mode='GUIDED')
                 print('4')
                 script = subprocess.Popen([sys.executable, GUIDED_MODE_SCRIPT], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
                 return ModeChangesResponse('Mode changed to guided')
@@ -82,8 +58,7 @@ def handle_mode_changes(req):
             return ModeChangesResponse('Mode guided already running') 
     elif mode_type in ['emergency_stop']:
         try:
-            emergency_srv = rospy.ServiceProxy('/mavros/set_mode', SetMode)
-            resp = emergency_srv(base_mode=0, custom_mode='HOLD')
+            resp = mode_change_srv(base_mode=0, custom_mode='HOLD')
             return ModeChangesResponse('Robot disarmed')
         except rospy.ServiceException as e:
             print("Service call failed: %s"%e)
