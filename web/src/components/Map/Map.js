fcb060c707e62138e673c15d97182498bfc391fa
CebulowyNinja <jan@olencki.pl>
2021-01-13 00:45:24 +0100

    Add basic map

--- web/src/components/Map/Map.js
+++ web/src/components/Map/Map.js
@@ -1,17 +1,31 @@
 import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "components/Map/Map.scss";
+import "components/Map/fixLeaflet.js";
+import { Button } from "react-bootstrap";
+import { TrashFill } from "react-bootstrap-icons";
 
 class Map extends React.Component {
   render() {
-    const containerStyle = {
-      display: "flex",
-      height: "100%",
-      justifyContent: "center",
-      alignItems: "center",
-      background: "rgba(150, 61, 124, 0.05)"
-    }
     return (
-      <div style={ containerStyle }>
-        <div>Map</div>
+      <div className="Map-container">
+        <div className="Map-header">
+          <Button variant="secondary">
+            <TrashFill/>
+          </Button>
+        </div>
+        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
       </div>
     )
   }
