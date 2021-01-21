6744f9cdf44d6b974d9095915fff6498ce574b9d
rosnuc <rosnuc@rosnuc.gmail>
2021-01-21 11:01:46 +0100

    after first tests

--- robot/src/waypoint_navigation/nodes/conv_geo_to_odom.py
+++ robot/src/waypoint_navigation/nodes/conv_geo_to_odom.py
@@ -1,7 +1,7 @@
 from pyproj import Proj, transform
 
 def conv_geo_to_odom(in_geo, ref_geo, ref_odom):
-    proj_utm = Proj(proj="utm", zone="32U", ellps="WGS84") # TODO: Change zone to Poland, Gdansk
+    proj_utm = Proj(proj="utm", zone="34", ellps="WGS84") # TODO: Change zone to Poland, Gdansk
     ref_cart = proj_utm(ref_geo[1], ref_geo[0]) # ref_cart[0] - Northing, ref_cart[1] - Easting
     in_cart = proj_utm(in_geo[1], in_geo[0])
     result = (in_cart[1] - ref_cart[1] + ref_odom[0], ref_cart[0] - in_cart[0] + ref_odom[1])
